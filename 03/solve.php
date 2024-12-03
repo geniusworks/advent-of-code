82b1ae7c8a29f53aee533f7065f7451935e3dc22
Martin Diekhoff <martin@geniusworks.com>
2024-12-02 23:22:29 -0800

    AOC2024: Update

--- 03/solve.php
+++ 03/solve.php
@@ -2,13 +2,6 @@
 
 // Part 1
 
-/*
- * Review and complete code to solve for the following:
- * The goal of the program is just to multiply some numbers. It does that with instructions like mul(X,Y), where X and Y are each 1-3 digit numbers. For instance, mul(44,46) multiplies 44 by 46 to get a result of 2024. Similarly, mul(123,4) would multiply 123 by 4.
- * However, because the program's memory has been corrupted, there are also many invalid characters that should be ignored, even if they look like part of a mul instruction. Sequences like mul(4*, mul(6,9!, ?(12,34), or mul ( 2 , 4 ) do nothing.
- * Scan the corrupted contents of input.txt for uncorrupted mul instructions. What do you get if you add up all of the results of the multiplications?
- */
-
 $lines = file('input.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
 $result = 0;
@@ -26,14 +19,6 @@ foreach ($lines as $line) {
 echo "Final result: $result\n";
 
 // Part 2
-
-/*
- * Consider that the previous code is 100% correct and produces the expected output.
- * But now for Part 2, let's consider that there are two new instructions you'll need to handle:
- * 1. The do() instruction enables future mul instructions.
- * 2. The don't() instruction disables future mul instructions.
- * Only the most recent do() or don't() instruction applies. At the beginning of the program, mul instructions are enabled.
- */
 
 $part2Result = 0;
 $isEnabled = true;
---
+++ 04/solve.php
@@ -0,0 +1,12 @@
+<?php
+
+// Part 1
+
+/*
+ *
+ */
+
+$lines = file('input.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+// Part 2
+
