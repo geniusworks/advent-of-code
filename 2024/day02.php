042eabd33a29c2b4d38d72f873e75a7ea3857b2b
Martin Diekhoff <martin@geniusworks.com>
2024-12-06 04:34:48 -0800

    AoC 2024 Day 02 cleanup

--- 2024/day02.php
+++ 2024/day02.php
@@ -1,24 +1,52 @@
 <?php
 
+// Advent of Code 2024 Day 2
+// Martin Diekhoff
+
+$start_time = microtime(true);
+$start_memory = memory_get_usage(true);
+
 $lines = file('input02.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
-// Part 1
-
 $safeReports = 0;
+$part2SafeReports = 0;
 
 foreach ($lines as $line) {
     $levels = array_map('intval', explode(' ', $line));
 
+    if (isValid($levels)) {
+        $safeReports++;
+    }
+
+    foreach ($levels as $j => $level) {
+        $newLevels = array_merge(array_slice($levels, 0, $j), array_slice($levels, $j + 1));
+
+        if (isValid($newLevels)) {
+            $part2SafeReports++;
+            break;
+        }
+    }
+}
+
+echo "Number of safe reports (Part 1): $safeReports" . PHP_EOL;
+echo "Number of safe reports (Part 2): $part2SafeReports" . PHP_EOL;
+
+$end_time = microtime(true);
+$end_memory = memory_get_usage(true);
+
+echo "Time elapsed: " . ($end_time - $start_time) . " seconds" . PHP_EOL;
+echo "Memory usage: " . ($end_memory - $start_memory) . " bytes" . PHP_EOL;
+
+function isValid($levels): bool
+{
     $isIncreasing = true;
     $isDecreasing = true;
-    $isValid = true;
 
     for ($i = 1; $i < count($levels); $i++) {
         $diff = abs($levels[$i] - $levels[$i - 1]);
 
         if ($diff < 1 || $diff > 3) {
-            $isValid = false;
-            break;
+            return false;
         }
 
         if ($levels[$i] > $levels[$i - 1]) {
@@ -28,49 +56,5 @@ foreach ($lines as $line) {
         }
     }
 
-    if ($isValid && ($isIncreasing || $isDecreasing)) {
-        $safeReports++;
-    }
+    return $isIncreasing || $isDecreasing;
 }
-
-// Part 2
-
-$part2SafeReports = 0;
-for ($i = 0; $i < count($lines); $i++) {
-    $levels = array_map('intval', explode(' ', $lines[$i]));
-
-    $isSafe = false;
-    for ($j = 0; $j < count($levels); $j++) {
-        $newLevels = array_merge(array_slice($levels, 0, $j), array_slice($levels, $j + 1));
-        $isIncreasing = true;
-        $isDecreasing = true;
-        $isValid = true;
-
-        for ($k = 1; $k < count($newLevels); $k++) {
-            $diff = abs($newLevels[$k] - $newLevels[$k - 1]);
-
-            if ($diff < 1 || $diff > 3) {
-                $isValid = false;
-                break;
-            }
-
-            if ($newLevels[$k] > $newLevels[$k - 1]) {
-                $isDecreasing = false;
-            } elseif ($newLevels[$k] < $newLevels[$k - 1]) {
-                $isIncreasing = false;
-            }
-        }
-
-        if ($isValid && ($isIncreasing || $isDecreasing)) {
-            $isSafe = true;
-            break;
-        }
-    }
-
-    if ($isSafe) {
-        $part2SafeReports++;
-    }
-}
-
-echo "Number of safe reports (Part 1): $safeReports" . PHP_EOL;
-echo "Number of safe reports (Part 2): $part2SafeReports" . PHP_EOL;
