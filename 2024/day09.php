cacdbbbbed66cfed4d47f77d2fdbb960389cdf78
Martin Diekhoff <martin@geniusworks.com>
2024-12-08 22:06:45 -0800

    AoC Day 9 Part 1

--- 2024/day09.php
+++ 2024/day09.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Advent of Code 2024
- * Day 9:
+ * Day 9: Disk Fragmenter
  *
  * @author Martin Diekhoff
  * @link https://adventofcode.com/2024/day/9
@@ -11,24 +11,69 @@ const DATA_INPUT_FILE = 'input09.txt';
 
 require_once __DIR__ . '/../' . 'bootstrap.php';
 
-$input = DataImporter::importFromFileWithDefaultFlags(__DIR__ . '/' . DATA_INPUT_FILE);
+$input = DataImporter::importFromFileContents(__DIR__ . '/' . DATA_INPUT_FILE);
 
-// ...
+function compactDisk($diskMap): float|int
+{
+    $blocks = [];
+    $fileId = 0;
+    $i = 0;
+    while ($i < strlen($diskMap)) {
+        if ($i + 1 >= strlen($diskMap)) {
+            $fileLength = (int)$diskMap[$i];
+            for ($j = 0; $j < $fileLength; $j++) {
+                $blocks[] = $fileId;
+            }
+            break;
+        }
+
+        $fileLength = (int)$diskMap[$i];
+        $freeSpaceLength = (int)$diskMap[$i + 1];
+
+        // Add file blocks for current file ID
+        for ($j = 0; $j < $fileLength; $j++) {
+            $blocks[] = $fileId;
+        }
+
+        // Add free space blocks
+        for ($j = 0; $j < $freeSpaceLength; $j++) {
+            $blocks[] = '.';
+        }
+
+        $fileId++;
+        $i += 2;
+    }
+
+    // Compact files
+    for ($i = count($blocks) - 1; $i >= 0; $i--) {
+        $block = $blocks[$i];
+        if ($block !== '.') {
+            for ($j = 0; $j < $i; $j++) {
+                if ($blocks[$j] === '.') {
+                    $blocks[$j] = $block;
+                    $blocks[$i] = '.';
+                    break;
+                }
+            }
+        }
+    }
+
+    // Calculate filesystem checksum
+    $checksum = 0;
+    foreach ($blocks as $index => $block) {
+        if ($block !== '.') {
+            $checksum += $index * $block;
+        }
+    }
+
+    return $checksum;
+}
 
 // Part 1
 
 $profiler = new Profiler('Part 1');
 $profiler->startProfile();
-$result1 = null; // TODO: Calculate the result for part 1.
+$result1 = compactDisk($input);
 $profiler->stopProfile();
-echo "Result = {$result1}" . PHP_EOL;
-$profiler->reportProfile();
-
-// Part 2
-
-$profiler = new Profiler('Part 2');
-$profiler->startProfile();
-$result2 = null; // TODO: Calculate the result for part 2.
-$profiler->stopProfile();
-echo "Result = {$result2}" . PHP_EOL;
+echo "Disk blocks checksum: {$result1}" . PHP_EOL;
 $profiler->reportProfile();
