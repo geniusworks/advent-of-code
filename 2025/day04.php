f6769029e9797953b624b416fcd44ca0d42efea6
Martin Diekhoff <martin@geniusworks.com>
2025-12-03 21:07:11 -0800

    AoC Day 4 Part 2

--- 2025/day04.php
+++ 2025/day04.php
@@ -71,7 +71,111 @@ function solvePart1(array $input)
 
 function solvePart2(array $input)
 {
-    return null;
+    $grid = [];
+
+    foreach ($input as $line) {
+        $line = rtrim($line, "\r\n");
+        if ($line === '') {
+            continue;
+        }
+        $grid[] = str_split($line);
+    }
+
+    $rows = count($grid);
+    if ($rows === 0) {
+        return 0;
+    }
+
+    $cols = count($grid[0]);
+
+    $dirs = [
+        [-1, -1], [-1, 0], [-1, 1],
+        [0, -1],           [0, 1],
+        [1, -1],  [1, 0],  [1, 1],
+    ];
+
+    $isRoll = [];
+    $counts = [];
+
+    for ($r = 0; $r < $rows; $r++) {
+        $isRoll[$r] = [];
+        $counts[$r] = [];
+        for ($c = 0; $c < $cols; $c++) {
+            $isRoll[$r][$c] = ($grid[$r][$c] === '@');
+            $counts[$r][$c] = 0;
+        }
+    }
+
+    for ($r = 0; $r < $rows; $r++) {
+        for ($c = 0; $c < $cols; $c++) {
+            if (!$isRoll[$r][$c]) {
+                continue;
+            }
+
+            $adjacentRolls = 0;
+
+            foreach ($dirs as [$dr, $dc]) {
+                $nr = $r + $dr;
+                $nc = $c + $dc;
+
+                if ($nr < 0 || $nr >= $rows || $nc < 0 || $nc >= $cols) {
+                    continue;
+                }
+
+                if ($grid[$nr][$nc] === '@') {
+                    $adjacentRolls++;
+                }
+            }
+
+            $counts[$r][$c] = $adjacentRolls;
+        }
+    }
+
+    $queue = [];
+    $head = 0;
+
+    for ($r = 0; $r < $rows; $r++) {
+        for ($c = 0; $c < $cols; $c++) {
+            if ($isRoll[$r][$c] && $counts[$r][$c] < 4) {
+                $queue[] = [$r, $c];
+            }
+        }
+    }
+
+    $removed = 0;
+
+    while ($head < count($queue)) {
+        [$r, $c] = $queue[$head++];
+
+        if (!$isRoll[$r][$c]) {
+            continue;
+        }
+
+        if ($counts[$r][$c] >= 4) {
+            continue;
+        }
+
+        $isRoll[$r][$c] = false;
+        $removed++;
+
+        foreach ($dirs as [$dr, $dc]) {
+            $nr = $r + $dr;
+            $nc = $c + $dc;
+
+            if ($nr < 0 || $nr >= $rows || $nc < 0 || $nc >= $cols) {
+                continue;
+            }
+
+            if ($isRoll[$nr][$nc]) {
+                $counts[$nr][$nc]--;
+                if ($counts[$nr][$nc] < 4) {
+                    $queue[] = [$nr, $nc];
+                }
+            }
+        }
+    }
+
+    return $removed;
 }
 
 $profiler = new Profiler();
